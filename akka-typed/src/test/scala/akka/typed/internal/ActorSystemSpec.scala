e38b3b0e45166b42c9f102217cfb6fbf684c365a
Roland Kuhn <rk@rkuhn.info>
2016-07-27 11:56:26 +0200

    shut down test ActorSystem also when failed

--- akka-typed/src/test/scala/akka/typed/internal/ActorSystemSpec.scala
+++ akka-typed/src/test/scala/akka/typed/internal/ActorSystemSpec.scala
@@ -7,10 +7,12 @@ package internal
 import org.junit.runner.RunWith
 import org.scalactic.ConversionCheckedTripleEquals
 import org.scalatest._
+import org.scalatest.exceptions.TestFailedException
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.concurrent.Eventually
 import scala.concurrent.duration._
 import scala.concurrent.{ Future, Promise }
+import scala.util.control.NonFatal
 
 @RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class ActorSystemSpec extends Spec with Matchers with BeforeAndAfterAll with ScalaFutures with Eventually with ConversionCheckedTripleEquals {
@@ -23,13 +25,26 @@ class ActorSystemSpec extends Spec with Matchers with BeforeAndAfterAll with Sca
   trait CommonTests {
     def system[T](name: String, props: Props[T]): ActorSystem[T]
 
+    def withSystem[T](name: String, props: Props[T], doTerminate: Boolean = true)(block: ActorSystem[T] => Unit): Terminated = {
+      val sys = system(name, props)
+      try {
+        block(sys)
+        if (doTerminate) sys.terminate().futureValue else sys.whenTerminated.futureValue
+      } catch {
+        case NonFatal(ex) =>
+          sys.terminate()
+          throw ex
+      }
+    }
+
     def `must start the guardian actor and terminate when it terminates`(): Unit = {
-      val sys = system("a", Props(Total[Probe] { p ⇒ p.replyTo ! p.msg; Stopped }))
-      val inbox = Inbox[String]("a")
-      sys ! Probe("hello", inbox.ref)
-      eventually { inbox.hasMessages should ===(true) }
-      inbox.receiveAll() should ===("hello" :: Nil)
-      val p = sys.whenTerminated.futureValue.ref.path
+      val t = withSystem("a", Props(Total[Probe] { p ⇒ p.replyTo ! p.msg; Stopped }), doTerminate = false) { sys =>
+        val inbox = Inbox[String]("a")
+        sys ! Probe("hello", inbox.ref)
+        eventually { inbox.hasMessages should ===(true) }
+        inbox.receiveAll() should ===("hello" :: Nil)
+      }
+      val p = t.ref.path
       p.name should ===("/")
       p.address.system should ===("a")
     }
@@ -47,46 +62,42 @@ class ActorSystemSpec extends Spec with Matchers with BeforeAndAfterAll with Sca
 
     def `must log to the event stream`(): Unit = pending
 
-    def `must have a name`(): Unit = {
-      val sys = system("name", Props(Empty))
-      sys.name should ===("name")
-      sys.terminate().futureValue
-    }
+    def `must have a name`(): Unit =
+      withSystem("name", Props(Empty[String])) { sys =>
+        sys.name should ===("name")
+      }
 
-    def `must report its uptime`(): Unit = {
-      val sys = system("uptime", Props(Empty))
-      val up = sys.uptime
-      up should be < 500L
-      Thread.sleep(300)
-      sys.uptime should be >= (up + 300)
-      sys.terminate().futureValue
-    }
+    def `must report its uptime`(): Unit =
+      withSystem("uptime", Props(Empty[String])) { sys =>
+        val up = sys.uptime
+        up should be < 500L
+        Thread.sleep(300)
+        sys.uptime should be >= (up + 300)
+      }
 
-    def `must have a working thread factory`(): Unit = {
-      val sys = system("thread", Props(Empty))
-      val p = Promise[Int]
-      sys.threadFactory.newThread(new Runnable {
-        def run(): Unit = p.success(42)
-      }).start()
-      p.future.futureValue should ===(42)
-      sys.terminate().futureValue
-    }
+    def `must have a working thread factory`(): Unit =
+      withSystem("thread", Props(Empty[String])) { sys =>
+        val p = Promise[Int]
+        sys.threadFactory.newThread(new Runnable {
+          def run(): Unit = p.success(42)
+        }).start()
+        p.future.futureValue should ===(42)
+      }
 
-    def `must be able to run Futures`(): Unit = {
-      val sys = system("thread", Props(Empty))
-      val f = Future(42)(sys.executionContext)
-      f.futureValue should ===(42)
-      sys.terminate().futureValue
-    }
+    def `must be able to run Futures`(): Unit =
+      withSystem("futures", Props(Empty[String])) { sys =>
+        val f = Future(42)(sys.executionContext)
+        f.futureValue should ===(42)
+      }
 
     // this is essential to complete ActorCellSpec, see there
-    def `must correctly treat Watch dead letters`(): Unit = {
-      val sys = system("deadletters", Props(Empty))
-      val client = new DebugRef[Int](sys.path / "debug", true)
-      sys.deadLetters.toImpl.sendSystem(Watch(sys, client))
-      client.receiveAll() should ===(Left(DeathWatchNotification(sys, null)) :: Nil)
-      sys.terminate().futureValue
-    }
+    def `must correctly treat Watch dead letters`(): Unit =
+      withSystem("deadletters", Props(Empty[String])) { sys =>
+        val client = new DebugRef[Int](sys.path / "debug", true)
+        sys.deadLetters.toImpl.sendSystem(Watch(sys, client))
+        client.receiveAll() should ===(Left(DeathWatchNotification(sys, null)) :: Nil)
+      }
+
   }
 
   object `An ActorSystemImpl` extends CommonTests {
